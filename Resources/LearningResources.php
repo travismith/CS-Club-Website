ce0012e1a1d5ccf9a3849ef828a65d734e11d08f
travismith <travismith112000@gmail.com>
2023-03-03 02:28:04 -0600

    3-3 Update
    - Added profile menu
    - Added more modulation to tutorials and newsletter

--- Resources/LearningResources.php
+++ Resources/LearningResources.php
@@ -11,180 +11,220 @@
 					<span class="InlineCenter">Learn about Fusion360 from local Minot engineer Jeremy Almond!</span>
 				</div>
 			</a>
-<?php	}
+<?php
+		}
 
-		function SoftwareDevelopment()
+function SoftwareDevelopment()
+{
+?>
+
+	<button class="DevTitle" onclick="Languages('ProgrammingLanguages')">
+		Learn new Languages
+	</button>
+	
+	<script>
+		function Languages(Topic)
 		{
-?>			<span class="DevTitle">Learn new Languages</span>
-			
-			<div class="TileWrapper">
-				<div class="TopicTile Orbitron">
-					<span class="InlineCenter">Programming Languages</span>
+			const TopicTile = document.querySelector('div.' + Topic);
+			if (TopicTile.style.display == "flex")
+			{
+				console.log("Closing");
+				TopicTile.style.display = "none";
+			}
+			else
+			{
+				console.log("Opening");
+				TopicTile.style.display = "flex";
+			}
+		}
+	</script>
 
-					<div class="TileBody">
-						<a class="Tutorial StartHere" href="?Selected=Learn&Interest=Software Development&Tutorial=ProgrammingBasics">Programming Basics -- Start here!</a>
-						<br>
-						
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=CPP">C++</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Python">Python</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=CSharp">C#</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Lua">Lua</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Java">Java</a>
-					</div>
+	<div class="TileWrapper ProgrammingLanguages">
+		<div class="TopicTile Orbitron">
+			<span class="InlineCenter">Programming Languages</span>
+
+			<div class="TileBody">
+				<a class="Tutorial StartHere" href="?Selected=Learn&Interest=Software Development&Tutorial=ProgrammingBasics">Programming Basics</a>
+				<br>
+				
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=CPP">C++</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Python">Python</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=CSharp">C#</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Lua">Lua</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Java">Java</a>
+			</div>
+		</div>
+
+		<div href="?Selected=Learn&Language=CPP" class="TopicTile Orbitron">
+			<span class="InlineCenter">Web Languages</span>
+
+			<div class="TileBodyRow">
+				<div>
+					<span class="ColumnTitle">Content</span>
+
+					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=HTML">HTML</a>
+					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=CSS">CSS</a>
 				</div>
 
-				<div href="?Selected=Learn&Language=CPP" class="TopicTile Orbitron">
-					<span class="InlineCenter">Web Languages</span>
-
-					<div class="TileBody">
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=HTML">HTML</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=CSS">CSS</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=JS">JavaScript</a>
-						
-						<div class="Indented">
-							<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=React">React</a>
-							<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Angular">Angular</a>
-							<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Node">Node</a>
-						</div>
-						
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=PHP">PHP</a>
-					</div>
-				</div>
-
-				<div class="TopicTile Orbitron">
-					<span class="InlineCenter">Database Languages</span>
-
-					<div class="TileBody">
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MySQL">MySQL</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MongoDB</a>
-					</div>
-				</div>
-
-				<div class="TopicTile Orbitron">
-					<span class="InlineCenter">Assembly Languages</span>
-
-					<div class="TileBody">
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MySQL">1086 Assembly</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MIPs Assembly</a>
-					</div>
+				<div>
+					<span class="ColumnTitle">Logic</span>
+					
+					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=PHP">PHP</a>
+					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=JS">JavaScript</a>
+				
+					<ul class="JSFrameworks">
+						<li><a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=React">React</a></li>
+						<li><a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Angular">Angular</a></li>
+						<li><a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Node">Node</a></li>
+					</ul>
 				</div>
 			</div>
+		</div>
 
-			<span class="DevTitle">Learn Developer Skills</span>
-			
-			<div class="TileWrapper">
-				<div class="TopicTile Orbitron">
-					<span class="InlineCenter">Source / Version Control</span>
+		<div class="TopicTile Orbitron">
+			<span class="InlineCenter">Database Languages</span>
 
-					<div class="TileBody">
-						<a class="Tutorial StartHere" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Github Desktop</a>
-						<br>
-						
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Git">Git</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Github Command Line</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Bitbucket</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Source Tree</a>
-					</div>
-				</div>
-
-				<a href="?Selected=Learn&Interest=System Administration" class="TopicTile Orbitron">
-					<span class="InlineCenter">System Administration</span>
-
-					<div class="TileBody">
-						<span class="InlineCenter">Networking</span>
-						<span class="InlineCenter">Command Line</span>
-						<span class="InlineCenter">Shell Scripting</span>
-					</div>
-				</a>
+			<div class="TileBody">
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MySQL">MySQL</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MongoDB</a>
 			</div>
+		</div>
 
-			<span class="DevTitle">Learn Web Application Stacks</span>
-			
-			<div class="TileWrapper">
-				<div class="TopicTile Orbitron">
-					<span class="InlineCenter">LAMP Stack</span>
+		<div class="TopicTile Orbitron">
+			<span class="InlineCenter">Assembly Languages</span>
 
-					<div class="TileBody">
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Linux">Linux</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=System Administration&Tutorial=Apache">Apache</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MySQL">MySQL</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=PHP">PHP</a>
-					</div>
-				</div>
-
-				<div class="TopicTile Orbitron">
-					<span class="InlineCenter">MEAN Stack</span>
-
-					<div class="TileBody">
-					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MongoDB</a>
-					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=ExpressJS">Express.js</a>
-					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=AngularJS">Angular.js</a>
-					<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=NodeJS">Node.js</a>
-					</div>
-				</div>
-
-				<div class="TopicTile Orbitron">
-					<span class="InlineCenter">MERN Stack</span>
-
-					<div class="TileBody">
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MongoDB</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=ExpressJS">Express.js</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=React">React</a>
-						<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=NodeJS">Node.js</a>
-					</div>
-				</div>
-
-				<a href="" class="TopicTile Orbitron">
-					<span class="InlineCenter">Ruby on Rails</span>
-
-					<div class="TileBody">
-						<span class="InlineCenter">MongoDB</span>
-						<span class="InlineCenter">Express.js</span>
-						<span class="InlineCenter">React</span>
-						<span class="InlineCenter">Node.js</span>
-					</div>
-				</a>
+			<div class="TileBody">
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MySQL">1086 Assembly</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MIPs Assembly</a>
 			</div>
+		</div>
+	</div>
+	
+	<button class="DevTitle" onclick="Languages('DevSkills')">
+		Learn Developer Skills
+	</button>
 
-			<span class="DevTitle">Learn Mobile Application Stacks</span>
-			
-			<div class="TileWrapper">
-				<a href="?Selected=Learn&Topic=LAMP" class="TopicTile Orbitron">
-					<span class="InlineCenter">LAMP Stack</span>
+	<div class="TileWrapper DevSkills">
+		<div class="TopicTile Orbitron">
+			<span class="InlineCenter">Source / Version Control</span>
 
-					<div class="TileBody">
-						<span class="InlineCenter">Linux</span>
-						<span class="InlineCenter">Apache</span>
-						<span class="InlineCenter">MySQL</span>
-						<span class="InlineCenter">PHP</span>
-					</div>
-				</a>
+			<div class="TileBody">
+				<a class="Tutorial StartHere" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Github Desktop</a>
+				<br>
+				
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Git">Git</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Github Command Line</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Bitbucket</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Github Desktop">Source Tree</a>
 			</div>
+		</div>
 
-			<span class="DevTitle">Learn Software Architectures</span>
+		<a href="?Selected=Learn&Interest=System Administration" class="TopicTile Orbitron">
+			<span class="InlineCenter">System Administration</span>
 
-			<div class="TileWrapper">
-				<a href="" class="TopicTile Orbitron">
-					<span class="InlineCenter">MVC Architecture</span>
-
-					<div class="TileBody">
-						<span class="InlineCenter">Model</span>
-						<span class="InlineCenter">View</span>
-						<span class="InlineCenter">Controller</span>
-					</div>
-				</a>
-
-				<a href="?Selected=Learn&Topic=LAMP" class="TopicTile Orbitron">
-					<span class="InlineCenter">MVC Architecture</span>
-
-					<div class="TileBody">
-						<span class="InlineCenter">Model</span>
-						<span class="InlineCenter">View</span>
-						<span class="InlineCenter">Controller</span>
-					</div>
-				</a>
+			<div class="TileBody">
+				<span class="InlineCenter">Networking</span>
+				<span class="InlineCenter">Command Line</span>
+				<span class="InlineCenter">Shell Scripting</span>
 			</div>
-<?php	}
+		</a>
+	</div>
+
+	<button class="DevTitle" onclick="Languages('WebStacks')">
+		Learn Web Application Stacks
+	</button>
+	
+	<div class="TileWrapper WebStacks">
+		<div class="TopicTile Orbitron">
+			<span class="InlineCenter">LAMP Stack</span>
+
+			<div class="TileBody">
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=Linux">Linux</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=System Administration&Tutorial=Apache">Apache</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MySQL">MySQL</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=PHP">PHP</a>
+			</div>
+		</div>
+
+		<div class="TopicTile Orbitron">
+			<span class="InlineCenter">MEAN Stack</span>
+
+			<div class="TileBody">
+			<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MongoDB</a>
+			<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=ExpressJS">Express.js</a>
+			<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=AngularJS">Angular.js</a>
+			<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=NodeJS">Node.js</a>
+			</div>
+		</div>
+
+		<div class="TopicTile Orbitron">
+			<span class="InlineCenter">MERN Stack</span>
+
+			<div class="TileBody">
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=MongoDB">MongoDB</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=ExpressJS">Express.js</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=React">React</a>
+				<a class="Tutorial" href="?Selected=Learn&Interest=Software Development&Tutorial=NodeJS">Node.js</a>
+			</div>
+		</div>
+
+		<a href="" class="TopicTile Orbitron">
+			<span class="InlineCenter">Ruby on Rails</span>
+
+			<div class="TileBody">
+				<span class="InlineCenter">MongoDB</span>
+				<span class="InlineCenter">Express.js</span>
+				<span class="InlineCenter">React</span>
+				<span class="InlineCenter">Node.js</span>
+			</div>
+		</a>
+	</div>
+	
+	<button class="DevTitle" onclick="Languages('MobileStacks')">
+		Learn Mobile Application Stacks
+	</button>
+	
+	<div class="TileWrapper MobileStacks">
+		<a href="?Selected=Learn&Topic=LAMP" class="TopicTile Orbitron">
+			<span class="InlineCenter">MobileStack Stack</span>
+
+			<div class="TileBody">
+				<span class="InlineCenter">Linux</span>
+				<span class="InlineCenter">Apache</span>
+				<span class="InlineCenter">MySQL</span>
+				<span class="InlineCenter">PHP</span>
+			</div>
+		</a>
+	</div>
+	
+	<button class="DevTitle" onclick="Languages('SoftwareArchitectures')">
+		Learn Software Architectures
+	</button>
+
+	<div class="TileWrapper">
+		<a href="" class="TopicTile Orbitron">
+			<span class="InlineCenter">MVC Architecture</span>
+
+			<div class="TileBody">
+				<span class="InlineCenter">Model</span>
+				<span class="InlineCenter">View</span>
+				<span class="InlineCenter">Controller</span>
+			</div>
+		</a>
+
+		<a href="?Selected=Learn&Topic=LAMP" class="TopicTile Orbitron">
+			<span class="InlineCenter">MVC Architecture</span>
+
+			<div class="TileBody">
+				<span class="InlineCenter">Model</span>
+				<span class="InlineCenter">View</span>
+				<span class="InlineCenter">Controller</span>
+			</div>
+		</a>
+	</div>
+
+<?php
+}
 
 		function AI()
 		{
